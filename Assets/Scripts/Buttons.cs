a8ddb029717898d36c6212e22d9c3fc5ee37d3a2
Paul Turaczyk <kantal.streamy@gmail.com>
2020-01-09 19:16:24 +0100

    Initial commit

---
+++ Assets/Scripts/Buttons.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Buttons : MonoBehaviour
+{
+    [Header("Canvas")]
+    [SerializeField] Canvas popup;
+
+    [Header("Text")]
+    [SerializeField] Text textToShow;
+
+    [Header("Sound")]
+    [SerializeField] AudioSource audio;
+    [SerializeField] AudioClip testSound;
+    [SerializeField] [Range(0, 1)] float testSoundVolume = 0.50f;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            Button1();
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            Button2();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Button3();
+        }
+    }
+
+    void Start()
+    {
+        textToShow.enabled = false;
+        audio = GetComponent<AudioSource>();
+        popup.enabled = false;
+    }
+
+    public void Button1 ()
+    {
+        Debug.Log("sound played");
+        audio.PlayOneShot(testSound, testSoundVolume);
+    }
+
+    public void Button2 ()
+    {
+        Debug.Log("popup showed");
+        popup.enabled = true;
+    }
+
+    public void Button3 ()
+    {
+        Debug.Log("text showed");
+        StartCoroutine(textShow());
+    }
+
+    IEnumerator textShow ()
+    {
+        textToShow.enabled = true;
+        yield return new WaitForSeconds(3);
+        textToShow.enabled = false;
+    }
+}
---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    [Header("Canvas")]
+    [SerializeField] Canvas popup;
+
+    public void ClosePopup()
+    {
+        popup.enabled = false;
+    }
+}
